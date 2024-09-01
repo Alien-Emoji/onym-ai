16a8159f0eb9103a4fb0dd6b1fdc804d230cce31
Alien-Emoji <Dap8805@gmail.com>
2024-09-01 01:41:51 -0700

    Transitioned from feature-engineered model to end-to-end model, updated README, and revised data processing script
    - Removed legacy code related to the feature-engineered model
    - Implemented the initial steps for transitioning to an end-to-end model
    - Updated README with new project information
    - Streamlined data processing script for end-to-end model integration
    - Updated requirements.txt to reflect current dependencies

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# onym-ai
+### Onym-AI
+Onym-AI is an AI model that forecasts the success of a YouTube thumbnail and title pair by predicting their Viewer-to-Subscriber Ratio (VSR).
+
+### Why VSR?
+Ideally, the optimal metric for the model to predict would be the video's Click-Through Rate (CTR), as it is a direct measurement of how many viewers click on a video after seeing it. Unfortunately, analytics surrounding CTR aren't readily available to the public, making a large-scale model challenging to create — especially for individuals like myself. As an alternative, Onym-AI predicts a video's VSR; a metric that gauges how well a video performs relative to the channel's subscriber base.
+
+### Initial Analysis and Correlation Findings
+Before committing to VSR as the predictive metric, I conducted an initial analysis using data from my own YouTube channel to assess its statistical viability. The results showed a moderate positive correlation between VSR and CTR (r ≈ 0.48, p ≈ 0.036). Although this level of correlation isn't ideal, it suggests that VSR can serve as a reasonable proxy for CTR in this application.
+
+Below is a scatter plot showing the relationship between VSR and CTR, revealing a moderately positive trendline:
+![Scatter Plot showing the Correlation between VSR and CTR, revealing a moderately positive trendline](images\Chart.webp)
+
+Although the initial analysis is based on a limited and unrefined dataset, it provides enough evidence for me to confidently proceed with the project. I aim to further explore and refine the correlation between VSR and CTR to improve the model's predictive accuracy.
+
+### Why is it called Onym?
+I named the model "Onym-AI" after the Greek suffix "-onym," which literally means "Name." I felt it was fitting for an AI related to YouTube titles and thumbnails.
+
+### Why create Onym?
+I've had the project idea for a while. Initially, I only wanted to predict the success of a video's thumbnail. However, after watching [this video by Learn By Leo](https://www.youtube.com/watch?v=CQeh_f2zh3k), I was inspired not only to start the project but also to include the title in the prediction. The project is entirely educational and non-commercial.
--- src/data_processing.py
+++ src/data_processing.py
@@ -1,22 +1,24 @@
 import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+import nltk.downloader
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import string
 
-import easyocr
+from gensim import corpora
+from gensim.models import LdaModel
+
 import pandas as pd
-import numpy as np
-from PIL import Image, ImageStat
 import requests
-import math
 import cv2
 import os
 
-# Initialize stuff
-nltk.download('vader_lexicon')
+# Download NLTK files
+nltk.download("punkt")
+nltk.download("stopwords")
+nltk.download("wordnet")
 
-sid = SentimentIntensityAnalyzer()
-reader = easyocr.Reader(['en'], gpu = True)
-
-# Clean the Data
+# Retrieve video thumbnail
 def get_highest_quality_thumbnail(video_id):
     thumbnail_url = f"https://i.ytimg.com/vi/{video_id}/maxresdefault.jpg"
     response = requests.get(thumbnail_url)
@@ -47,77 +49,32 @@ def save_thumbnail(video_id):
             
     return thumbnail_path
 
-# Extract title features: Length, Keywords, Sentiment
-def get_title_NLP_features(title):
-    tokens = nltk.word_tokenize(title)
-    sentiment = sid.polarity_scores(title)
+# Determine video topic
+def preprocess_text(text):
+    tokens = word_tokenize(text)
+    tokens = [word for word in tokens if word not in string.punctuation and word not in stopwords.words("english")]
     
-    return (tokens, sentiment)
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    
+    return tokens
 
-# Extract thumbnail features: Color Analysis (brightness, saturation, contrast), Text Presence, Object Detection
-def get_thumbnail_attributes(thumbnail_path):
-    thumbnail = Image.open(thumbnail_path)
-    thumbnail_np = np.array(thumbnail)
-    
-    # Get perceived brightness value (from https://stackoverflow.com/a/3498247/26443787)
-    stat = ImageStat.Stat(thumbnail)
-    r,g,b = stat.mean[:3]
-    perceived_brightness = math.sqrt(
-        0.241 * (r ** 2) +
-        0.691 * (g ** 2) +
-        0.068 * (b ** 2)
-    )
-    
-    # Get the thumbnail's mean saturation value
-    hsv = cv2.cvtColor(thumbnail_np, cv2.COLOR_RGB2HSV)
-    saturation = hsv[:, :, 1].mean()
-    
-    # Get the thumbnail's contrast value
-    grayscale = cv2.cvtColor(thumbnail_np, cv2.COLOR_RGB2GRAY)
-    contrast = grayscale.std()
-    
-    return (perceived_brightness, saturation, contrast)
-
-def detect_thumbnail_text(thumbnail_path):
-    thumbnail = cv2.imread(thumbnail_path)
-    results = reader.readtext(thumbnail)
-    return " ".join([text for _, text, _ in results])
-
-def detect_thumbnail_objects(thumbnail_path):
+def extract_topics():
     None
-
-# Title & Thumbnail synergy score:
-
-# Normalize data
 
 # Read the CSV File
 def read_csv(csv):
     df = pd.read_csv(csv)
     processed_videos = []
     
-    for index, row in df.iterrows():       
+    for _, row in df.iterrows():       
         video = {}
         video_id = row["video_id"]
         video_title = row["title"]
         
-        thumbnail_path = save_thumbnail(video_id)
-        brightness, saturation, contrast = get_thumbnail_attributes(thumbnail_path)
-        tokens, sentiment = get_title_NLP_features(video_title)
-        thumbnail_text = detect_thumbnail_text(thumbnail_path)
-        
-        video['title'] = video_title # For debugging, remove later
+        video['title'] = video_title
+        video['thumbnail'] = save_thumbnail(video_id)
         video['vsr'] = row['vsr']
-        video['title_length'] = len(tokens)
-        video['title_neg'] = sentiment['neg']
-        video['title_neu'] = sentiment['neu']
-        video['title_pos'] = sentiment['pos']
-        video['title_abs_compound'] = abs(sentiment['compound']) # We use the absolute sentiment compound to gauge how much emotion is in the title
-        video['thumbnail_brightnses'] = brightness
-        video['thumbnail_saturation'] = saturation
-        video['thumbnail_contrast'] = contrast
-        
-        if thumbnail_text:
-            print(f"({video_id}) Text detected in the thumbnail: {thumbnail_text}") # For debugging
         
         processed_videos.append(video)
             
