9e55dc4bc79d5bc366471feb6a8cc2f0de3705fb
Alien-Emoji <Dap8805@gmail.com>
2024-09-02 01:39:59 -0700

    Implemented topic modeling
    - Titles & Descriptions get preprocessed
    - Tags get processed and take into account entities
    - Links get filtered out
    - Updated requirements.txt

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,22 +1,13 @@
-import nltk
-from nltk.corpus import stopwords
-import nltk.downloader
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-import string
-
-from gensim import corpora
-from gensim.models import LdaModel
-
 import pandas as pd
 import requests
 import cv2
 import os
+import re
+import ast
+from collections import Counter
 
-# Download NLTK files
-nltk.download("punkt")
-nltk.download("stopwords")
-nltk.download("wordnet")
+import spacy
+nlp = spacy.load("en_core_web_sm")
 
 # Retrieve video thumbnail
 def get_highest_quality_thumbnail(video_id):
@@ -45,36 +36,108 @@ def save_thumbnail(video_id):
         if "hqdefault" in thumbnail_url:
             thumbnail = cv2.imread(thumbnail_path)
             cropped_image = thumbnail[45:315, 0:480] # Crop to 480x270 (16:9 aspect ratio)
-            cv2.imwrite(thumbnail_path, cropped_image)
+            resized_image = cv2.resize(cropped_image, (1280, 720), interpolation = cv2.INTER_AREA) # Resize to match with maxresdefault
+            cv2.imwrite(thumbnail_path, resized_image)
             
     return thumbnail_path
 
 # Determine video topic
-def preprocess_text(text):
-    tokens = word_tokenize(text)
-    tokens = [word for word in tokens if word not in string.punctuation and word not in stopwords.words("english")]
-    
-    lemmatizer = WordNetLemmatizer()
-    tokens = [lemmatizer.lemmatize(word) for word in tokens]
-    
-    return tokens
+def is_url_token(token):
+    url_patterns = ["http", "www", ".com", ".net", ".org", ".io", ".gov", ".edu", ".ly"]
+    return any(pattern in token for pattern in url_patterns)
 
-def extract_topics():
-    None
+def preprocess_text(text):
+    entity_pass = nlp(text)
+    entities = [entity.text for entity in entity_pass.ents]
+    
+    # Remove entities from this pass
+    filtered_text = text
+    for entity in entities:
+        filtered_text = filtered_text.replace(entity, "")
+        
+    filtered_text = " ".join(filtered_text.split())
+    
+    nlp_text = nlp(filtered_text)
+    tokens = [
+        token.lemma_.lower()
+        for token in nlp_text
+        if not token.is_stop and not token.is_punct and len(token) > 2 and not is_url_token(token.text.lower())
+    ]
+    
+    return (tokens, entities)
+
+def process_tags(tag_list, entities):
+    # Move longer entities to the front.
+    # If there are any entities contained in others, these entities will be longer and therefore come first in the list.
+    lowercase_entities = sorted([entity.lower() for entity in entities], key=len, reverse=True)
+    
+    refined_tags = []
+    for tag in tag_list:
+        entity_found = False
+        
+        for entity in lowercase_entities:
+            if entity == tag:
+                refined_tags.append(entity)
+                entity_found = True
+                break
+            
+            if entity in tag:
+                # Split the entities from the rest of the string and extend results to the list
+                segments = re.split(rf"({re.escape(entity)})", tag)
+                for segment in segments:
+                    segment = segment.strip()
+                    
+                    if segment and segment == entity:
+                        refined_tags.append(segment)
+                    else:
+                        # Tokenize non-entity segments
+                        nlp_text = nlp(segment)
+                        tokens = [token.lemma_.lower() for token in nlp_text if not token.is_stop and not token.is_punct]
+                        refined_tags.extend(tokens)
+                
+                entity_found = True
+                break # Stop checking for other entities when a result is found
+            
+        if not entity_found:
+            # Tokenize entire tag if no entity found
+            nlp_text = nlp(tag)
+            tokens = [token.lemma_.lower() for token in nlp_text if not token.is_stop and not token.is_punct and token.text.strip()]
+            refined_tags.append(tag)
+    
+    refined_tags = [tag for tag in refined_tags if tag.strip()] # Remove empty elements & excessive whitespace
+    return refined_tags
+
+def determine_topic(topic_tokens, top_n=3):
+    token_frequency = Counter(topic_tokens)
+    sorted_tokens = [token for token, _ in token_frequency.most_common()]
+    
+    if sorted_tokens:
+        return sorted_tokens[:top_n]
+    else:
+        return ["unknown"]
 
 # Read the CSV File
 def read_csv(csv):
     df = pd.read_csv(csv)
     processed_videos = []
     
-    for _, row in df.iterrows():       
+    for _, row in df.iterrows():
         video = {}
         video_id = row["video_id"]
-        video_title = row["title"]
+        title = row["title"]
         
-        video['title'] = video_title
-        video['thumbnail'] = save_thumbnail(video_id)
-        video['vsr'] = row['vsr']
+        video["title"] = title
+        video["thumbnail"] = save_thumbnail(video_id)
+        video["vsr"] = row["vsr"]
+                
+        title_tokens, title_entities = preprocess_text(title)
+        desc_tokens, desc_entities = preprocess_text(row["description"])
+        entities = title_entities + desc_entities
+        
+        tags_list = ast.literal_eval(row["tags"]) # Make sure the tags are in list format (sometimes it's stored as a string in csv files)
+        refined_tags = process_tags(tags_list, entities)
+        topic_tokens = [token.lower() for token in title_tokens + desc_tokens + refined_tags]
+        video["topic"] = determine_topic(topic_tokens)
         
         processed_videos.append(video)
             
